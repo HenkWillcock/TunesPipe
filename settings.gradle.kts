4a6fe78c3db160891bd1d5fb6370f98d12944892
Henk Willcock <henk.willcock@outlook.co.nz>
2025-11-10 16:08:21 +1300

    ASD

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,27 +1,38 @@
-import org.gradle.kotlin.dsl.maven
+@file:Suppress("UnstableApiUsage")
 
-pluginManagement {
-    repositories {
-        google {
-            content {
-                includeGroupByRegex("com\\.android.*")
-                includeGroupByRegex("com\\.google.*")
-                includeGroupByRegex("androidx.*")
-            }
-        }
-        mavenCentral()
-        gradlePluginPortal()
-        maven(url = "https://jitpack.io")
-    }
-}
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+
     repositories {
         google()
         mavenCentral()
-        maven(url = "https://jitpack.io")
+        maven { setUrl("https://jitpack.io") }
     }
 }
 
-rootProject.name = "TunesPipe"
+// F-Droid doesn't support foojay-resolver plugin
+// plugins {
+//     id("org.gradle.toolchains.foojay-resolver-convention") version("1.0.0")
+// }
+
+rootProject.name = "Metrolist"
 include(":app")
+include(":innertube")
+include(":kugou")
+include(":lrclib")
+
+// Use a local copy of NewPipe Extractor by uncommenting the lines below.
+// We assume, that Metrolist and NewPipe Extractor have the same parent directory.
+// If this is not the case, please change the path in includeBuild().
+//
+// For this to work you also need to change the implementation in innertube/build.gradle.kts
+// to one which does not specify a version.
+// From:
+//      implementation(libs.newpipe.extractor)
+// To:
+//      implementation("com.github.teamnewpipe:NewPipeExtractor")
+//includeBuild("../NewPipeExtractor") {
+//    dependencySubstitution {
+//        substitute(module("com.github.teamnewpipe:NewPipeExtractor")).using(project(":extractor"))
+//    }
+//}
